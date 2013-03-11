0c4b241dc24d9e2c43355feeff337c4740218902
gmerencio.santos@gmail.com <gmerencio.santos@gmail.com@ff7211c2-70b9-e154-77c6-c17a8a42dab7>
2013-03-11 18:17:35 +0000

    Remove everything for update.

--- trunk/src/br/edu/ifsp/lp2/shadowstruggles/games/package-info.java
+++
@@ -1,4 +0,0 @@
-/***
- * Classes representing each battle of the game. They derive from {@link br.edu.ifsp.lp2.shadowstruggles.screens.BattleScreen}.
- */
-package br.edu.ifsp.lp2.shadowstruggles.games;
