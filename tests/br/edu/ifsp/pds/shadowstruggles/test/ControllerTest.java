51e8be0451bca855a72158322b4072fd58f609f3
lhsakurai <lhsakurai@gmail.com>
2013-03-22 00:54:29 +0000

    JUnit - ControllerTest (empty)

---
+++ tests/br/edu/ifsp/pds/shadowstruggles/test/ControllerTest.java
@@ -0,0 +1,84 @@
+package br.edu.ifsp.pds.shadowstruggles.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ControllerTest {
+
+	@Test
+	public void testMapClicked() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBackCardClicked() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPentagramClicked() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHandCardClicked() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeckClicked() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMenuButtonClicked() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlayerLifeChanged() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEnemyLifeChanged() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlayerEnergyChanged() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEnemyEnergyChanged() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTileChanged() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSummonFighter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateTimer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveCard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVerifyValueChange() {
+		fail("Not yet implemented");
+	}
+
+}
