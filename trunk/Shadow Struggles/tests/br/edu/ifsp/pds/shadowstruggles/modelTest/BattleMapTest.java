5d2a7fe5b1ced1789ad8b481c5b6e9dc2f413b83
leite.juliano@gmail.com <leite.juliano@gmail.com@ff7211c2-70b9-e154-77c6-c17a8a42dab7>
2013-05-10 01:33:38 +0000

    testes

---
+++ trunk/Shadow Struggles/tests/br/edu/ifsp/pds/shadowstruggles/modelTest/BattleMapTest.java
@@ -0,0 +1,68 @@
+package br.edu.ifsp.pds.shadowstruggles.modelTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import br.edu.ifsp.pds.shadowstruggles.model.BattleMap;
+import br.edu.ifsp.pds.shadowstruggles.model.Card;
+
+public class BattleMapTest {
+	
+	@Test
+	public void testCardOnMap() {
+		//método pode estar com erro
+		Card card = new Card("Carta", "carta", 10, "carta", 10, null);
+		Card card2 = new Card("Carta2", "carta2", 10, "carta2", 10, null);
+		BattleMap map = new BattleMap("mapa");
+		map.addCard(card, 20, 2);
+		assertTrue(map.cardOnMap(card, 2, 1));
+		assertFalse(map.cardOnMap(card2, 3, 1));
+	}
+	
+	@Test
+	public void testEnemyBaseAttacked() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEnemyCards() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetPlayerCards() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testLaneWithLessAllies() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testLaneWithMoreEnemies() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testLaneWithMoreEnemiesInvading() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testNextAvailable() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testNextAvailableTile() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPlayerBaseAttacked() {
+		fail("Not yet implemented");
+	}
+	
+}
---
+++ trunk/Shadow Struggles/tests/br/edu/ifsp/pds/shadowstruggles/modelTest/BattleTest.java
@@ -0,0 +1,18 @@
+package br.edu.ifsp.pds.shadowstruggles.modelTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BattleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+	
+}
