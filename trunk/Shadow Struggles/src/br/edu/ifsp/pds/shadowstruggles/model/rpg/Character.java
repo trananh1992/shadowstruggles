606ed3e6b3d95cd5e19e828d35581a7ea5b93eee
hugo.mitsumori@gmail.com <hugo.mitsumori@gmail.com@ff7211c2-70b9-e154-77c6-c17a8a42dab7>
2013-05-13 13:04:12 +0000

    corrigido bug de character andando pra fora do mapa

--- trunk/Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/model/rpg/Character.java
+++ trunk/Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/model/rpg/Character.java
@@ -6,38 +6,61 @@ public class Character {
 	private Profile profile;
 	private int tileX;
 	private int tileY;
-	public static enum WalkDirection{
-		WALK_UP,WALK_DOWN, WALK_LEFT,WALK_RIGHT;
+
+	public static enum WalkDirection {
+		WALK_UP, WALK_DOWN, WALK_LEFT, WALK_RIGHT;
 	}
-	
+
 	public Character(Profile profile) {
-		this.profile=profile;
+		this.profile = profile;
 	}
-	
+
 	public Character(Profile profile, int tileX, int tileY) {
-		this.profile=profile;
-		this.tileX=tileX;
-		this.tileY=tileY;
-		
+		this.profile = profile;
+		this.tileX = tileX;
+		this.tileY = tileY;
+
 	}
-	
-	
-	public void walk(WalkDirection direction, RpgMap map){
+
+	public boolean walk(WalkDirection direction, RpgMap map) {
 		map.getTile(tileX, tileY).removeCharacer();
-		switch(direction){
-		case WALK_UP:tileY++;break;
-			case WALK_DOWN:tileY--;break;
-			case WALK_LEFT:tileX--;break;
-			case WALK_RIGHT:tileX++;break;
-		default:break;
+		boolean walked = false;
+		switch (direction) {
+		case WALK_UP:
+			if (tileY < map.getHeight() - 1) {
+				tileY++;
+				walked = true;
+			}
+			break;
+		case WALK_DOWN:
+			if (tileY > 0) {
+				tileY--;
+				walked = true;
+			}
+			break;
+		case WALK_LEFT:
+			if (tileX > 0) {
+				tileX--;
+				walked = true;
+			}
+			break;
+		case WALK_RIGHT:
+			if (tileX < map.getWidth() - 1) {
+				tileX++;
+				walked = true;
+			}
+			break;
+		default:
+			break;
 		}
 		map.getTile(tileX, tileY).insertCharacter(this);
+		return walked;
 	}
 
 	public int getTileX() {
 		return tileX;
 	}
-	
+
 	public int getTileY() {
 		return tileY;
 	}
