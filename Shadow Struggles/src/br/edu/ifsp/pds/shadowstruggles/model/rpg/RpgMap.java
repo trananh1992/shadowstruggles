07482222e756c1f76967de7c2979ae25bb8ee7a1
hugo.mitsumori <hugo.mitsumori@gmail.com>
2013-05-13 13:04:12 +0000

    corrigido bug de character andando pra fora do mapa

--- Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/model/rpg/RpgMap.java
+++ Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/model/rpg/RpgMap.java
@@ -7,9 +7,13 @@ import com.badlogic.gdx.utils.Array;
 public class RpgMap {
 	private Array<Array<Tile>> tiles;
 	private Array<Item> items;
+	private int width;
+	private int height;
 	
 	public RpgMap(int width, int height) {
 		tiles= new Array<Array<Tile>>();
+		this.width=width;
+		this.height=height;
 		for(int i = 0;i < width;i++){
 			tiles.add(new Array<Tile>());
 			for(int j = 0 ; j< height; j++)
@@ -22,6 +26,14 @@ public class RpgMap {
 	
 	public Tile getTile(int tileX, int tileY){
 		return tiles.get(tileX).get(tileY);
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public int getWidth() {
+		return width;
 	}
 	
 	
