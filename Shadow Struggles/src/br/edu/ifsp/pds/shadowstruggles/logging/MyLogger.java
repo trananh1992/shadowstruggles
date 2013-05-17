dd0f6bb041c70173c720ec975dc7985278c44e68
gmerencio.santos <gmerencio.santos@gmail.com>
2013-05-17 00:17:46 +0000

    Copyright comments.

--- Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/logging/MyLogger.java
+++ Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/logging/MyLogger.java
@@ -7,29 +7,32 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
+// Based on a Java Logging API tutorial by Lars Vogel, version 1.3:
+// http://www.vogella.com/articles/Logging/article.html.
+
 public class MyLogger {
-  static private FileHandler fileTxt;
-  static private SimpleFormatter formatterTxt;
+	static private FileHandler fileTxt;
+	static private SimpleFormatter formatterTxt;
 
-  static private FileHandler fileHTML;
-  static private Formatter formatterHTML;
+	static private FileHandler fileHTML;
+	static private Formatter formatterHTML;
 
-  static public void setup() throws IOException {
+	static public void setup() throws IOException {
 
-    // Get the global logger to configure it
-    Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+		// Get the global logger to configure it
+		Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-    logger.setLevel(Level.INFO);
-    fileTxt = new FileHandler("data/Logging.txt");
-    fileHTML = new FileHandler("data/Logging.html");
-    // Create txt Formatter
-    formatterTxt = new SimpleFormatter();
-    fileTxt.setFormatter(formatterTxt);
-    logger.addHandler(fileTxt);
+		logger.setLevel(Level.INFO);
+		fileTxt = new FileHandler("data/Logging.txt");
+		fileHTML = new FileHandler("data/Logging.html");
+		// Create txt Formatter
+		formatterTxt = new SimpleFormatter();
+		fileTxt.setFormatter(formatterTxt);
+		logger.addHandler(fileTxt);
 
-    // Create HTML Formatter
-    formatterHTML = new MyHtmlFormatter();
-    fileHTML.setFormatter(formatterHTML);
-    logger.addHandler(fileHTML);
-  }
-} 
+		// Create HTML Formatter
+		formatterHTML = new MyHtmlFormatter();
+		fileHTML.setFormatter(formatterHTML);
+		logger.addHandler(fileHTML);
+	}
+}
