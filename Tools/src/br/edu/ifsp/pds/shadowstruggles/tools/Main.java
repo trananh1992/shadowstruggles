e2ac8fe707a40498f916ad388fc2f024873c79d2
gmerencio.santos <gmerencio.santos@gmail.com>
2013-05-17 00:08:10 +0000

    Small problems fixed.

--- Tools/src/br/edu/ifsp/pds/shadowstruggles/tools/Main.java
+++ Tools/src/br/edu/ifsp/pds/shadowstruggles/tools/Main.java
@@ -5,7 +5,7 @@ import br.edu.ifsp.pds.shadowstruggles.tools.view.Window;
 public class Main {
 
 	public static void main(String[] args) {
-		Window window = new Window();
+		new Window();
 	}
 
 }
--- Tools/src/br/edu/ifsp/pds/shadowstruggles/tools/view/Window.java
+++ Tools/src/br/edu/ifsp/pds/shadowstruggles/tools/view/Window.java
@@ -1,7 +1,5 @@
 package br.edu.ifsp.pds.shadowstruggles.tools.view;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
