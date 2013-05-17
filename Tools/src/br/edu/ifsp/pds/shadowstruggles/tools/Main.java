eae7ab5ba2c99ecc3358a016ccb2570b6c33e990
gmerencio.santos <gmerencio.santos@gmail.com>
2013-05-17 00:58:51 +0000

    Started implementation of logging system.

--- Tools/src/br/edu/ifsp/pds/shadowstruggles/tools/Main.java
+++ Tools/src/br/edu/ifsp/pds/shadowstruggles/tools/Main.java
@@ -1,11 +1,38 @@
 package br.edu.ifsp.pds.shadowstruggles.tools;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import br.edu.ifsp.pds.shadowstruggles.tools.data.DataHelperTest;
+import br.edu.ifsp.pds.shadowstruggles.tools.utils.MyLogger;
 import br.edu.ifsp.pds.shadowstruggles.tools.view.Window;
 
 public class Main {
 
 	public static void main(String[] args) {
-		new Window();
+//		Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+//
+//		logger.setLevel(Level.INFO);
+//		try {
+//			FileHandler fileTxt = new FileHandler("Logging.txt");
+//			SimpleFormatter formatterTxt = new SimpleFormatter();
+//			fileTxt.setFormatter(formatterTxt);
+//			logger.addHandler(fileTxt);
+//		} catch (SecurityException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		Exception e = new Exception("Erro besta");
+//		logger.info("Teste");
+		
+		 new Window();
 	}
 
 }
