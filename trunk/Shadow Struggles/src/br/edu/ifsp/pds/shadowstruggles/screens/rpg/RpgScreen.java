8f6cd3b3b04021ab20ee53c4ab1266fc1cee79d8
leonhpb@gmail.com <leonhpb@gmail.com@ff7211c2-70b9-e154-77c6-c17a8a42dab7>
2013-05-18 17:13:55 +0000

    Quadrado de teste adicionado na RpgScreen para programar os movimentos do Character na tela.

--- trunk/Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/screens/rpg/RpgScreen.java
+++ trunk/Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/screens/rpg/RpgScreen.java
@@ -19,75 +19,141 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
-
-
-public class RpgScreen extends BaseScreen{
+public class RpgScreen extends BaseScreen {
 	private TiledMap map;
 	private Character character;
-	
-	public RpgScreen(ShadowStruggles game, Controller controller, RpgPlatform rpgPlatform) {
+	private ShapeRenderer shapeRenderer;
+
+	private boolean readyToWalk = true;
+	private float walked = 0;
+	private WalkDirection direction = null;
+	private int tileSize = 60;
+
+	public RpgScreen(ShadowStruggles game, Controller controller,
+			RpgPlatform rpgPlatform) {
 		super(game, controller);
 		this.map = rpgPlatform.getMap();
 		character = new Character(game.getProfile());
+		shapeRenderer = new ShapeRenderer();
 	}
-	
+
 	@Override
-	public void render(float delta) {		
+	public void render(float delta) {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		float unitScale = 1 / 256f;
-		OrthogonalTiledMapRenderer renderer = new OrthogonalTiledMapRenderer(map, unitScale);
+		OrthogonalTiledMapRenderer renderer = new OrthogonalTiledMapRenderer(
+				map, unitScale);
 		renderer.setView(camera);
 		camera.setToOrtho(false, 3.75f, 2.5f);
 		renderer.render();
 		update(delta);
-	}
-	
-	public void update(float delta){
-		keyInput(delta);
-	} 
 
-	public void keyInput(float delta){
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+		shapeRenderer.begin(ShapeType.Filled);
+
+		if (walked + character.getWalkSpeed() >= tileSize) {
+			readyToWalk = true;
+			walked = 0;
+			direction = null;
+		}
+
+		if (readyToWalk) {
+			shapeRenderer.rect(character.getTileX() * tileSize,
+					character.getTileY() * tileSize, tileSize, tileSize);
+		} else {
+
+			walked = walked + character.getWalkSpeed();
+			System.out.println(":\nReady to walk: " + readyToWalk
+					+ "\nWalked: " + walked + "\nDirection: " + direction
+					+ "\nCharacter speed: " + character.getWalkSpeed() + "\n");
+			switch (direction) {
+			case WALK_UP:
+				shapeRenderer.rect(character.getTileX() * tileSize,
+						character.getTileY() * tileSize + walked -  tileSize,
+						tileSize, tileSize);
+				break;
+			case WALK_DOWN:
+				shapeRenderer.rect(character.getTileX() * tileSize,
+						character.getTileY() * tileSize - walked +  tileSize,
+						tileSize, tileSize);
+				break;
+			case WALK_LEFT:
+				shapeRenderer.rect(character.getTileX() * tileSize
+						- walked + tileSize, character.getTileY() * tileSize, tileSize,
+						tileSize);
+				break;
+			case WALK_RIGHT:
+				shapeRenderer.rect(character.getTileX() * tileSize
+						+ walked - tileSize, character.getTileY() * tileSize, tileSize,
+						tileSize);
+				break;
+			default:
+				break;
+			}
+		}
+		shapeRenderer.end();
+
+	}
+
+	public void update(float delta) {
+		keyInput(delta);
+	}
+
+	public void keyInput(float delta) {
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
 			moveCharacter(WalkDirection.WALK_LEFT);
-			System.out.println("LEFT");
+			// System.out.println("Left:\nReady to walk: " + readyToWalk +
+			// "\nWalked: "
+			// + walked + "\nDirection: " + direction + "\n\n");
 		}
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
 			moveCharacter(WalkDirection.WALK_RIGHT);
-			System.out.println("RIGHT");
+			// System.out.println("Right:\nReady to walk: " + readyToWalk +
+			// "\nWalked: "
+			// + walked + "\nDirection: " + direction + "\n\n");
 		}
-		if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
 			moveCharacter(WalkDirection.WALK_UP);
-			System.out.println("UP");
+			// System.out.println("Up:\nReady to walk: " + readyToWalk +
+			// "\nWalked: "
+			// + walked + "\nDirection: " + direction + "\n\n");
 		}
-		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
 			moveCharacter(WalkDirection.WALK_DOWN);
-			System.out.println("DOWN");
+			// System.out.println("Down:\nReady to walk: " + readyToWalk +
+			// "\nWalked: "
+			// + walked + "\nDirection: " + direction + "\n\n");
 		}
 	}
-	
-	public void moveCharacter (WalkDirection direction){
-		character.walk(direction, map);
-		
-		switch (direction) {
-		case WALK_UP:
-			
-			break;
-		case WALK_DOWN:
-			
-			break;
-		case WALK_LEFT:
-			
-			break;
-		case WALK_RIGHT:
-			
-			break;
-		default:
-			break;
+
+	public void moveCharacter(WalkDirection direction) {
+		if (readyToWalk && character.walk(direction, map)) {
+			readyToWalk = false;
+			this.direction = direction;
+
+			switch (direction) {
+			case WALK_UP:
+
+				break;
+			case WALK_DOWN:
+
+				break;
+			case WALK_LEFT:
+
+				break;
+			case WALK_RIGHT:
+
+				break;
+			default:
+				break;
+			}
+			// System.out.println("moveCharacter:\nReady to walk: " +
+			// readyToWalk + "\nWalked: "
+			// + walked + "\nDirection: " + direction + "\n\n");
 		}
-				
 	}
-		
-	
+
 }
