c536933ed5320ed7ad505f023b03fe8717efba18
gmerencio.santos@gmail.com <gmerencio.santos@gmail.com@ff7211c2-70b9-e154-77c6-c17a8a42dab7>
2013-05-24 01:32:11 +0000

    Messing around a little bit with interfaces.

---
+++ trunk/Tools/src/br/edu/ifsp/pds/shadowstruggles/tools/view/DatabaseEditor.java
@@ -0,0 +1,54 @@
+package br.edu.ifsp.pds.shadowstruggles.tools.view;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+
+public class DatabaseEditor extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Create the frame.
+	 */
+	public DatabaseEditor() {
+		setTitle("Database Editor");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(0, 0, 434, 262);
+		contentPane.add(tabbedPane);
+		
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("Cards", null, panel, null);
+		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("Events", null, panel_1, null);
+		
+		JPanel panel_2 = new JPanel();
+		tabbedPane.addTab("Battles", null, panel_2, null);
+		
+		JPanel panel_3 = new JPanel();
+		tabbedPane.addTab("Rules", null, panel_3, null);
+		
+		JPanel panel_4 = new JPanel();
+		tabbedPane.addTab("Card Effects", null, panel_4, null);
+		
+		JPanel panel_5 = new JPanel();
+		tabbedPane.addTab("Decks", null, panel_5, null);
+		
+		JPanel panel_6 = new JPanel();
+		tabbedPane.addTab("Enemies", null, panel_6, null);
+	}
+}
--- trunk/Tools/src/br/edu/ifsp/pds/shadowstruggles/tools/view/Window.java
+++ trunk/Tools/src/br/edu/ifsp/pds/shadowstruggles/tools/view/Window.java
@@ -1,20 +1,22 @@
 package br.edu.ifsp.pds.shadowstruggles.tools.view;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Window {
 
 	private JFrame frmTitle;
-	private JTextField textField;
 
 	/**
 	 * Create the application.
 	 */
 	public Window() {
 		initialize();
+		frmTitle.setVisible(true);
 	}
 
 	/**
@@ -22,22 +24,62 @@ public class Window {
 	 */
 	private void initialize() {
 		frmTitle = new JFrame();
-		frmTitle.setTitle("Title");
+		frmTitle.setTitle("Shadow Struggles Editor");
 		frmTitle.setBounds(100, 100, 450, 300);
 		frmTitle.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmTitle.getContentPane().setLayout(null);
 		
-		JLabel lblLabel = new JLabel("Label");
-		lblLabel.setBounds(59, 110, 46, 14);
-		frmTitle.getContentPane().add(lblLabel);
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 434, 21);
+		frmTitle.getContentPane().add(menuBar);
 		
-		textField = new JTextField();
-		textField.setBounds(139, 107, 86, 20);
-		frmTitle.getContentPane().add(textField);
-		textField.setColumns(10);
+		JMenu mnNewMenu = new JMenu("File");
+		mnNewMenu.setMnemonic('F');
+		menuBar.add(mnNewMenu);
 		
-		JButton btnOk = new JButton("OK");
-		btnOk.setBounds(278, 106, 89, 23);
-		frmTitle.getContentPane().add(btnOk);
+		JMenuItem mntmNewFile = new JMenuItem("New File");
+		mnNewMenu.add(mntmNewFile);
+		
+		JMenuItem mntmOpen = new JMenuItem("Open File");
+		mnNewMenu.add(mntmOpen);
+		
+		JMenuItem mntmSave = new JMenuItem("Save File");
+		mnNewMenu.add(mntmSave);
+		
+		JMenuItem mntmClose = new JMenuItem("Close File");
+		mnNewMenu.add(mntmClose);
+		
+		JMenu mnDatabase = new JMenu("Database");
+		menuBar.add(mnDatabase);
+		
+		JMenuItem mntmEditDatabase = new JMenuItem("Edit Database");
+		mntmEditDatabase.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				new DatabaseEditor().setVisible(true);
+			}
+		});
+		mnDatabase.add(mntmEditDatabase);
+		
+		JMenuItem mntmClearDatabase = new JMenuItem("Clear Database");
+		mnDatabase.add(mntmClearDatabase);
+		
+		JMenu mnResources = new JMenu("Resources");
+		menuBar.add(mnResources);
+		
+		JMenuItem mntmEditResources = new JMenuItem("Edit Resources");
+		mnResources.add(mntmEditResources);
+		
+		JMenuItem mntmClearResources = new JMenuItem("Clear Resources");
+		mnResources.add(mntmClearResources);
+		
+		JMenu mnLanguages = new JMenu("Languages");
+		menuBar.add(mnLanguages);
+		
+		JMenuItem mntmEditLanguages = new JMenuItem("Edit Languages");
+		mnLanguages.add(mntmEditLanguages);
+		
+		JMenuItem mntmClearLanguages = new JMenuItem("Clear Languages");
+		mnLanguages.add(mntmClearLanguages);
 	}
 }
