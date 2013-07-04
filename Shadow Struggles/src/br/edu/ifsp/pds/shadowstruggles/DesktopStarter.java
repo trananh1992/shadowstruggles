77c2b2074c0e04063be546694f5571f993820da3
gmerencio.santos <gmerencio.santos@gmail.com>
2013-07-04 19:19:53 +0000

    Improved version of Main Screen implemented.

--- Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/DesktopStarter.java
+++ Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/DesktopStarter.java
@@ -11,7 +11,7 @@ public class DesktopStarter {
 	/**
 	 * The application's entry point for desktop platforms.
 	 */
-	
+
 	public static void main(String[] args) {
 		// TexturePacker2.process("data/images/skin", "data/images/skin",
 		// "skin");
@@ -29,17 +29,10 @@ public class DesktopStarter {
 		// "cards");
 		// TexturePacker2.process("data/images/char", "data/images/char",
 		// "char");
+		// TexturePacker2.process("data/images/char", "data/images/char",
+		// "char");
+		// System.out.println("Finish!");
 
-//		TexturePacker2.process("data/images/skin", "data/images/skin", "skin");
-//		TexturePacker2.process("data/images/game_ui_images", "data/images/game_ui_images", "game_ui_images");
-//        TexturePacker2.process("data/images/card_attacking", "data/images/card_attacking", "card_attacking");
-//        TexturePacker2.process("data/images/card_effects", "data/images/card_effects", "card_effects");
-//        TexturePacker2.process("data/images/card_images", "data/images/card_images", "card_images");
-//        TexturePacker2.process("data/images/card_walking", "data/images/card_walking", "card_walking");
-//        TexturePacker2.process("data/images/cards", "data/images/cards", "cards");
-//		TexturePacker2.process("data/images/char", "data/images/char", "char");
-//		System.out.println("Finish!");
-		
 		ApplicationListener listener = new ShadowStruggles(RunMode.DEBUG);
 		String title = "Shadow Struggles";
 		int width = 960, height = 640;
