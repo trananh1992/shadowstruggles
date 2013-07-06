eb053538524568298f724258e3cfe11185f84705
gmerencio.santos@gmail.com <gmerencio.santos@gmail.com@ff7211c2-70b9-e154-77c6-c17a8a42dab7>
2013-07-06 12:48:03 +0000

    Event model updated (trigger type).

--- trunk/Tools/src/br/edu/ifsp/pds/shadowstruggles/tools/model/events/ComplexEvent.java
+++ trunk/Tools/src/br/edu/ifsp/pds/shadowstruggles/tools/model/events/ComplexEvent.java
@@ -6,16 +6,17 @@ import br.edu.ifsp.pds.shadowstruggles.tools.model.quests.Quest;
 
 public class ComplexEvent extends Event {
 	public ArrayList<Event> events;
-	
+
 	public ComplexEvent() {
 		super();
 		this.events = new ArrayList<Event>();
 	}
-	
+
 	public ComplexEvent(int id, float x, float y, String map, String layer,
-			Quest quest, boolean triggered, String sprite, ArrayList<Event> events) {
-		super(id, x, y, map, layer, quest, triggered, sprite);
-		
+			Quest quest, boolean triggered, String sprite,
+			TriggerType triggerType, ArrayList<Event> events) {
+		super(id, x, y, map, layer, quest, triggered, sprite, triggerType);
+
 		this.events = events;
 	}
 }
