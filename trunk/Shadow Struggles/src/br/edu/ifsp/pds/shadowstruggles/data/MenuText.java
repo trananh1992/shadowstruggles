9f314caffa43eb35298f9b22ea8aff264bff0473
gmerencio.santos@gmail.com <gmerencio.santos@gmail.com@ff7211c2-70b9-e154-77c6-c17a8a42dab7>
2013-07-08 15:45:12 +0000

    Changes in ShopScreen.

--- trunk/Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/data/MenuText.java
+++ trunk/Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/data/MenuText.java
@@ -8,15 +8,14 @@ import com.badlogic.gdx.utils.JsonValue;
  * Class for storage of all text shown in the GUI, for purposes of i18n.
  */
 public class MenuText implements Serializable {
-
-	public String languageName;
-
-	// Start Screen
+	// General use
+	public String returnToStart = "";
+	
+	// StartScreen
 	public String continueGame = "";
 	public String newGame = "";
 
 	// Select Profile
-	public String returnToStart = "";
 	public String profile = "";
 
 	// Error Screen
@@ -26,8 +25,6 @@ public class MenuText implements Serializable {
 	// MainScreen
 	public String campaign = "";
 	public String freePlay = "";
-	public String configurations = "";
-	public String changeProfile = "";
 	public String editDeck = "";
 	public String shop = "";
 
@@ -36,19 +33,27 @@ public class MenuText implements Serializable {
 	public String continueButton = "";
 	public String mainMenuButton = "";
 
-	// InGameScreen
+	// InGameMenu
 	public String returnToGame = "";
 	public String exit = "";
 	public String checkCards = "";
 
 	// SettingsScreen
+	public String configurations = "";
 	public String volume = "";
 	public String music = "";
 	public String on = "";
 	public String off = "";
 	public String languageSelection = "";
 
-	// defeatScreen
+	// ShopScreen
+	public String buy = "";
+	public String sell = "";
+	public String packs = "";
+	public String cards = "";
+	public String extra = "";
+	
+	// DefeatScreen
 	public String defeat = "";
 	public String retryButton = "";
 
@@ -60,67 +65,121 @@ public class MenuText implements Serializable {
 
 	@Override
 	public void write(Json json) {
+		// General use
+		json.writeValue("returnToStart", returnToStart);
+		
+		// StartScreen
 		json.writeValue("continueGame", continueGame);
 		json.writeValue("newGame", newGame);
-		json.writeValue("returnToStart", returnToStart);
+		
+		// Select Profile
 		json.writeValue("profile", profile);
+		
+		// ErrorScreen
 		json.writeValue("terminate", terminate);
 		json.writeValue("details", details);
+		
+		// MainScreen
 		json.writeValue("campaign", campaign);
 		json.writeValue("freePlay", freePlay);
-		json.writeValue("configurations", configurations);
-		json.writeValue("changeProfile", changeProfile);
 		json.writeValue("editDeck", editDeck);
 		json.writeValue("shop", shop);
+		
+		// VictoryScreen
 		json.writeValue("victory", victory);
 		json.writeValue("continueButton", continueButton);
 		json.writeValue("mainMenuButton", mainMenuButton);
+		
+		// InGameMenu
 		json.writeValue("returnToGame", returnToGame);
 		json.writeValue("exit", exit);
 		json.writeValue("checkCards", checkCards);
+		
+		// SettingsScreen
+		json.writeValue("configurations", configurations);
 		json.writeValue("volume", volume);
 		json.writeValue("music", music);
 		json.writeValue("on", on);
 		json.writeValue("off", off);
 		json.writeValue("languageSelection", languageSelection);
+		
+		// ShopScreen
+		json.writeValue("buy", buy);
+		json.writeValue("sell", sell);
+		json.writeValue("packs", packs);
+		json.writeValue("cards", cards);
+		json.writeValue("extra", extra);
+		
+		// DefeatScreen
 		json.writeValue("retryButton", retryButton);
 		json.writeValue("defeat", defeat);
+		
+		// FreePlayScreen
 		json.writeValue("practiceBattle", practiceBattle);
+		
+		// EditDeckScreen
 		json.writeValue("newDeck", newDeck);
 	}
 
 	@Override
 	public void read(Json json, JsonValue jsonData) {
+		// General use
+		returnToStart = json.readValue("returnToStart", String.class, jsonData);
+		
+		// StartScreen
 		continueGame = json.readValue("continueGame", String.class, jsonData);
 		newGame = json.readValue("newGame", String.class, jsonData);
-		returnToStart = json.readValue("returnToStart", String.class, jsonData);
+		
+		// Select Profile
 		profile = json.readValue("profile", String.class, jsonData);
-		terminate = json.readValue("terminate", String.class, jsonData);
-		details = json.readValue("details", String.class, jsonData);
+		
+		// MainScreen
 		campaign = json.readValue("campaign", String.class, jsonData);
 		freePlay = json.readValue("freePlay", String.class, jsonData);
-		configurations = json.readValue("configurations", String.class,
-				jsonData);
-		changeProfile = json.readValue("changeProfile", String.class, jsonData);
 		editDeck = json.readValue("editDeck", String.class, jsonData);
 		shop = json.readValue("shop", String.class, jsonData);
+		
+		// ErrorScreen
+		terminate = json.readValue("terminate", String.class, jsonData);
+		details = json.readValue("details", String.class, jsonData);
+
+		// VictoryScreen
 		victory = json.readValue("victory", String.class, jsonData);
 		continueButton = json.readValue("continueButton", String.class,
 				jsonData);
 		mainMenuButton = json.readValue("mainMenuButton", String.class,
 				jsonData);
+		
+		// InGameMenu
 		returnToGame = json.readValue("returnToGame", String.class, jsonData);
 		exit = json.readValue("exit", String.class, jsonData);
 		checkCards = json.readValue("checkCards", String.class, jsonData);
+		
+		// SettingsScreen
+		configurations = json.readValue("configurations", String.class,
+				jsonData);
 		volume = json.readValue("volume", String.class, jsonData);
 		music = json.readValue("music", String.class, jsonData);
 		on = json.readValue("on", String.class, jsonData);
 		off = json.readValue("off", String.class, jsonData);
 		languageSelection = json.readValue("languageSelection", String.class, jsonData);
+		
+		// ShopScreen
+		buy = json.readValue("buy", String.class, jsonData);
+		sell = json.readValue("sell", String.class, jsonData);
+		packs = json.readValue("packs", String.class, jsonData);
+		cards = json.readValue("cards", String.class, jsonData);
+		extra = json.readValue("extra", String.class, jsonData);
+		
+		// DefeatScreen
 		defeat = json.readValue("defeat", String.class, jsonData);
 		retryButton = json.readValue("retryButton", String.class, jsonData);
+		
+		// FreePlayScreen
 		practiceBattle = json.readValue("practiceBattle", String.class,
 				jsonData);
+		
+		// EditDeckScreen
 		newDeck = json.readValue("newDeck", String.class, jsonData);
 	}
 
--- trunk/Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/screens/ShopScreen.java
+++ trunk/Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/screens/ShopScreen.java
@@ -3,6 +3,7 @@ package br.edu.ifsp.pds.shadowstruggles.screens;
 import br.edu.ifsp.pds.shadowstruggles.Controller;
 import br.edu.ifsp.pds.shadowstruggles.ShadowStruggles;
 import br.edu.ifsp.pds.shadowstruggles.ShadowStruggles.RunMode;
+import br.edu.ifsp.pds.shadowstruggles.data.dao.MenuTextDAO;
 import br.edu.ifsp.pds.shadowstruggles.data.dao.ProfileDAO;
 import br.edu.ifsp.pds.shadowstruggles.model.Card;
 import br.edu.ifsp.pds.shadowstruggles.model.Shop;
@@ -12,7 +13,6 @@ import br.edu.ifsp.pds.shadowstruggles.screens.utils.ScreenUtils;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
@@ -23,73 +23,94 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Array;
 
+// TODO: Acrescentar label para o dinheiro.
 public class ShopScreen extends BaseScreen {
+
+	private static enum Category {
+		PACK, EXTRA, CARD
+	};
+
+	private static enum Mode {
+		BUY, SELL
+	};
 
 	private BaseScreen previousScreen;
 	private Shop shop;
-	private Image selectedImage;
-	private Label name;
-	private Label description;
-	private Label price;
-	private Label moneyLabel;
+
 	private Arrow right;
 	private Arrow left;
 	private TextButton exit;
-	private TextButton buyButton;
-	private Card selectedCard;
-	private Array<Card> cards;
-	private Array<Image> cardImages;
 	private TextButton packsButton;
 	private TextButton extraButton;
 	private TextButton cardsButton;
-	private TextButton sellButton;
+	private TextButton buySellButton;
+
+	private Mode currentMode;
 
 	public ShopScreen(ShadowStruggles game, Controller controller,
 			BaseScreen previousScreen) {
 		super(game, controller);
+
 		this.previousScreen = previousScreen;
 		this.shop = new Shop(game);
+		this.currentMode = Mode.BUY;
+
 		initComponents();
 	}
 
 	private void initComponents() {
 		final BaseScreen menu = this.previousScreen;
-		
+
 		Table menuTable = new Table();
 		menuTable.defaults().padTop(10).width(160).height(50);
 
 		if (game.getMode() == RunMode.DEBUG)
 			menuTable.debug();
-		
-		packsButton = new TextButton("Packs", getSkin());
-		packsButton = ScreenUtils.defineButton(packsButton, 0, 0, 0, 0, super.getSkin());
-		packsButton.addListener(new ClickListener(){@Override
-		public void clicked(InputEvent event, float x, float y) {
-			changeShop(1);
-		}});
-		
-		extraButton = new TextButton("Extra", getSkin());
-		extraButton = ScreenUtils.defineButton(extraButton, 0, 0, 0, 0, super.getSkin());
-		extraButton.addListener(new ClickListener(){@Override
-		public void clicked(InputEvent event, float x, float y) {
-			changeShop(2);
-		}});
-		
-		cardsButton = new TextButton("Cards", getSkin());
-		cardsButton = ScreenUtils.defineButton(cardsButton, 0, 0, 0, 0, super.getSkin());
-		cardsButton.addListener(new ClickListener(){@Override
-		public void clicked(InputEvent event, float x, float y) {
-			changeShop(3);
-		}});
-		
-		sellButton = new TextButton("Sell", getSkin());
-		sellButton = ScreenUtils.defineButton(sellButton, 0, 0, 0, 0, super.getSkin());
-		sellButton.addListener(new ClickListener(){@Override
-		public void clicked(InputEvent event, float x, float y) {
-			changeShop(4);
-		}});
-		
-		exit = new TextButton("Back", getSkin());
+
+		packsButton = new TextButton(MenuTextDAO.getMenuText().packs, getSkin());
+		packsButton = ScreenUtils.defineButton(packsButton, 0, 0, 0, 0,
+				super.getSkin());
+		packsButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				changeCategory(Category.PACK);
+			}
+		});
+
+		extraButton = new TextButton(MenuTextDAO.getMenuText().extra, getSkin());
+		extraButton = ScreenUtils.defineButton(extraButton, 0, 0, 0, 0,
+				super.getSkin());
+		extraButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				changeCategory(Category.EXTRA);
+			}
+		});
+
+		cardsButton = new TextButton(MenuTextDAO.getMenuText().cards, getSkin());
+		cardsButton = ScreenUtils.defineButton(cardsButton, 0, 0, 0, 0,
+				super.getSkin());
+		cardsButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				changeCategory(Category.CARD);
+			}
+		});
+
+		String buySell = currentMode == Mode.BUY ? MenuTextDAO.getMenuText().sell
+				: MenuTextDAO.getMenuText().buy;
+		buySellButton = new TextButton(buySell, getSkin());
+		buySellButton = ScreenUtils.defineButton(buySellButton, 0, 0, 0, 0,
+				super.getSkin());
+		buySellButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				changeMode(Mode.SELL);
+			}
+		});
+
+		exit = new TextButton(MenuTextDAO.getMenuText().returnToStart,
+				getSkin());
 		exit = ScreenUtils.defineButton(exit, 0, 0, 0, 0, super.getSkin());
 		exit.addListener(new ClickListener() {
 
@@ -101,84 +122,61 @@ public class ShopScreen extends BaseScreen {
 			}
 		});
 
-		buyButton = new TextButton("Buy", getSkin());// TODO: string Buy p/
-														// arquivo
-		buyButton = ScreenUtils.defineButton(buyButton, 0, 0, 0, 0,
-				super.getSkin());
-		buyButton.addListener(new ClickListener() {
-
-			@Override
-			public void clicked(InputEvent event, float x, float y) {
-				game.getAudio().playSound("button_6");
-				shop.buyCard(selectedCard);
-				moneyLabel.setText("Money: $ "
-						+ String.valueOf(shop.getPlayerMoney()));// TODO: string
-																	// Money
-																	// para
-																	// arquivo
-			}
-		});
-		
 		menuTable.add(packsButton);
 		menuTable.row();
 		menuTable.add(extraButton);
 		menuTable.row();
 		menuTable.add(cardsButton);
 		menuTable.row();
-		menuTable.add(sellButton);
+		menuTable.add(buySellButton);
 		menuTable.row();
-		menuTable.add(buyButton);
+		menuTable.add(exit).padTop(150);
 		menuTable.row();
-		menuTable.add(exit);
-		menuTable.row();
-		menuTable.setPosition(100, 300);
-		
+		menuTable.setPosition(100, 350);
+
 		stage.addActor(menuTable);
-		
-		
-		
+
 		Table cardsTable = new Table();
-		cardsTable.defaults().width(150).height(120);
-		
 		if (game.getMode() == RunMode.DEBUG)
 			cardsTable.debug();
-		
-		ImageButton cardImgButton = new ImageButton(this.getSkin().getDrawable("placeholder"));
+		cardsTable.setPosition(570, 200);
 
-		name = new Label("Carta Teste", super.getSkin());
-		name.setWrap(true);
-		name.setStyle(new LabelStyle(super.getSkin().getFont("andalus-font"),
-				Color.WHITE));
-		
-		price = new Label("1000", super.getSkin());
-		price.setStyle(new LabelStyle(super.getSkin().getFont("andalus-font"),
-				Color.WHITE));
-		
-		cardsTable.add(cardImgButton);
-		cardsTable.row().height(40);;
-		cardsTable.add(name);
-		cardsTable.row().height(40);
-		cardsTable.add(price);
-		
-		cardsTable.setPosition(350, 500);
+		Array<Card> cards = shop.getAvailableCards();
+		// Auxiliary variable for adding rows in the table as necessary.
+		int aux = 0;
+		for (Card card : cards) {
+			Image cardImage = new Image(game.getTextureRegion(card.getName()
+					.toLowerCase(), "cards"));
+			ImageButton cardImgButton = new ImageButton(cardImage.getDrawable());
+
+			Label name = new Label(card.getNameVisualization(), super.getSkin());
+			name.setWrap(true);
+			name.setStyle(new LabelStyle(super.getSkin()
+					.getFont("andalus-font"), Color.WHITE));
+
+			Label price = new Label("1000", super.getSkin());
+			price.setStyle(new LabelStyle(super.getSkin().getFont(
+					"andalus-font"), Color.WHITE));
+
+			Table cardTable = new Table();
+			cardTable.defaults().width(150).height(120);
+			if (game.getMode() == RunMode.DEBUG)
+				cardTable.debug();
+			cardTable.add(cardImgButton);
+			cardTable.row().height(40);
+			cardTable.add(name);
+			cardTable.row().height(40);
+			cardTable.add(price);
+
+			if (aux == 3) {
+				cardsTable.row();
+				aux = 0;
+			}
+			cardsTable.add(cardTable);
+			aux++;
+		}
 		stage.addActor(cardsTable);
-		
-//		description = new Label("", super.getSkin());
-//		description.setWrap(true);
-//		description.setStyle(new LabelStyle(super.getSkin().getFont(
-//				"andalus-font"), Color.BLACK));
 
-//		moneyLabel = new Label("", super.getSkin());
-//		moneyLabel.setText("Money: $ " + String.valueOf(shop.getPlayerMoney()));
-//		// TODO: string Money para arquivo
-//		moneyLabel.setWidth(200);
-//		moneyLabel.setHeight(50);
-//		moneyLabel.setStyle(new LabelStyle(super.getSkin().getFont(
-//				"andalus-font"), Color.BLACK));
-
-		
-		
-		
 		Table rightButtonTable = new Table();
 		if (game.getMode() == RunMode.DEBUG)
 			rightButtonTable.debug();
@@ -189,7 +187,7 @@ public class ShopScreen extends BaseScreen {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
 				game.getAudio().playSound("button_6");
-				moveCards(1);
+				moveItems(1);
 			}
 		});
 		rightButtonTable.add(right);
@@ -205,95 +203,35 @@ public class ShopScreen extends BaseScreen {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
 				game.getAudio().playSound("button_6");
-				moveCards(-1);
-
+				moveItems(-1);
 			}
 		});
 		leftButtonTable.add(left).left();
 		leftButtonTable.setPosition(250, 300);
-		
+
 		stage.addActor(leftButtonTable);
 		stage.addActor(rightButtonTable);
-
-//		cards = shop.getAvailableCards();
-//		int count = 0;
-//		cardImages = new Array<Image>();
-//
-//		for (Card card : cards) {
-//			Image cardImage = new Image(game.getAssets()
-//					.get("data/images/cards/cards.atlas", TextureAtlas.class)
-//					.findRegion(card.getName().toLowerCase()));
-//			cardImage.setY(10);
-//			cardImage.setX(180 + count * 135);
-//			cardImage.setScaleX(0.5f);
-//			cardImage.setScaleY(0.5f);
-//			final Card card2 = card;
-//			cardImage.addListener(new ClickListener() {
-//
-//				@Override
-//				public void clicked(InputEvent event, float x, float y) {
-//					game.getAudio().playSound("button_2");
-//					changeCard(card2);
-//				}
-//			});
-//			cardImages.add(cardImage);
-//			if (cardImage.getX() >= 180 && cardImage.getX() < 800)
-//				stage.addActor(cardImage);
-//			count++;
-//		}
-//		changeCard(cards.get(0));
 	}
 
-	protected void changeShop(int tela) {
+	/**
+	 * Changes the current category.
+	 */
+	private void changeCategory(Category category) {
 		// TODO Efetuar a troca de tela quando clicar em um item do menu
-		
 	}
 
-	private void moveCards(int side) {
-		boolean movableToRight = false, movableToLeft = false;
-		for (Image card : cardImages) {
-			if (card.getX() < 180)
-				movableToRight = true;
-			if (card.getX() > 780)
-				movableToLeft = true;
-		}
-
-		if ((side > 0 && movableToLeft) || (side < 0 && movableToRight))
-			for (Image card : cardImages) {
-				card.setX(card.getX() - 135 * side);
-				if (card.getX() >= 135 && card.getX() < 800)
-					stage.addActor(card);
-				else {
-					try {
-						stage.removeActor(card);
-
-					} catch (Exception e) {
-					}
-				}
-			}
-
+	/**
+	 * Changes to another visualization mode (buying, selling).
+	 */
+	private void changeMode(Mode mode) {
+		// TODO: Implementar método.
 	}
 
-	private void changeCard(Card card) {
-		this.selectedCard = card;
-		description.setText(card.getDescription());
-		description.setHeight(description.getPrefHeight());
-		description.setY(590 - description.getHeight());
-		try {
-			stage.removeActor(selectedImage);
-		} catch (Exception e) {
-		}
-		selectedImage = new Image(game.getAssets()
-				.get("data/images/cards/cards.atlas", TextureAtlas.class)
-				.findRegion(card.getName().toLowerCase()));
-		selectedImage.setX(50);
-		selectedImage.setY(256);
-		selectedImage.setScaleX(1.3f);
-		selectedImage.setScaleY(1.3f);
-		name.setText(selectedCard.getNameVisualization());
-		name.setY(590);
-		stage.addActor(selectedImage);
-		buyButton.setText("Buy ($ " + card.getBuyCost() + ")");
+	/**
+	 * Shows additional or previous items from the selected category.
+	 */
+	private void moveItems(int side) {
+		// TODO: Implementar método.
 	}
 
 	@Override
