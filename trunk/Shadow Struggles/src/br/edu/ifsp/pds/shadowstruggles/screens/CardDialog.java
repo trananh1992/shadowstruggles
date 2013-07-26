a9e49985a054f57ba9ca558022c5651aaac41ba4
lhsakurai@gmail.com <lhsakurai@gmail.com@ff7211c2-70b9-e154-77c6-c17a8a42dab7>
2013-07-26 00:39:18 +0000

    CardDialog implemented in ShopScreen.

--- trunk/Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/screens/CardDialog.java
+++ trunk/Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/screens/CardDialog.java
@@ -1,13 +1,17 @@
 package br.edu.ifsp.pds.shadowstruggles.screens;
 
 import br.edu.ifsp.pds.shadowstruggles.ShadowStruggles;
+import br.edu.ifsp.pds.shadowstruggles.ShadowStruggles.RunMode;
 import br.edu.ifsp.pds.shadowstruggles.model.Card;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 /**
  * Dialog for showing a card's details.
@@ -16,10 +20,11 @@ public class CardDialog extends Dialog {
 	private ShadowStruggles game;
 	private Card card;
 	private Skin skin;
-	private Image box;
 	private Image cardImage;
 	private Label name;
 	private Label description;
+	private TextButton okButton;
+	private ScrollPane scroll;
 
 	public CardDialog(ShadowStruggles game, Card card, String title, Skin skin) {
 		super(title, skin);
@@ -49,9 +54,13 @@ public class CardDialog extends Dialog {
 	}
 
 	private void initComponents() {
+		Table inTable = new Table();
+		inTable.defaults().height(100).width(220);
+		if (game.getMode() == RunMode.DEBUG)
+			inTable.debug();
+
 		this.cardImage = new Image(game.getTextureRegion(card.getName()
 				.toLowerCase(), "cards"));
-		this.box = new Image(skin.getDrawable("box"));
 
 		this.name = new Label(card.getNameVisualization(), skin);
 		this.name.setColor(Color.BLACK);
@@ -60,11 +69,18 @@ public class CardDialog extends Dialog {
 		this.description = new Label(card.getDescription(), skin);
 		this.description.setColor(Color.BLACK);
 		this.description.setWrap(true);
+		
+		this.okButton = new TextButton("Ok", skin);
+		
+		scroll = new ScrollPane(description, skin);
 
-		this.add(cardImage);
-		this.add(box);
-		this.add(name);
-		this.add(description);
-		this.button("Ok", true);
+		inTable.add(this.name).height(50);
+		inTable.row().height(350);
+		inTable.add(scroll);
+		inTable.add(this.cardImage).padRight(30);
+		inTable.row();
+		inTable.add(this.okButton).height(50).colspan(2).center();
+		
+		this.add(inTable);
 	}
 }
