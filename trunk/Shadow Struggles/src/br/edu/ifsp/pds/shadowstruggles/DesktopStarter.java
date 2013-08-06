28c378aef3518b0dce2f89174581ec4bc615ca1f
gmerencio.santos@gmail.com <gmerencio.santos@gmail.com@ff7211c2-70b9-e154-77c6-c17a8a42dab7>
2013-08-06 00:32:49 +0000

    HTML logging removed.

--- trunk/Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/DesktopStarter.java
+++ trunk/Shadow Struggles/src/br/edu/ifsp/pds/shadowstruggles/DesktopStarter.java
@@ -1,13 +1,8 @@
 package br.edu.ifsp.pds.shadowstruggles;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.logging.FileHandler;
-import java.util.logging.Formatter;
-import java.util.logging.Handler;
 import java.util.logging.Level;
-import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
@@ -43,9 +38,6 @@ public class DesktopStarter {
 		static private FileHandler fileTxt;
 		static private SimpleFormatter formatterTxt;
 
-		static private FileHandler fileHTML;
-		static private Formatter formatterHTML;
-
 		public MyApplication(ApplicationListener listener, String title,
 				int width, int height, boolean useGL2) throws IOException {
 			super(listener, title, width, height, useGL2);
@@ -54,16 +46,10 @@ public class DesktopStarter {
 
 			logger.setLevel(Level.INFO);
 			fileTxt = new FileHandler("Logging.txt", true);
-			fileHTML = new FileHandler("Logging.html", true);
-			// Create txt Formatter
+			
 			formatterTxt = new SimpleFormatter();
 			fileTxt.setFormatter(formatterTxt);
 			logger.addHandler(fileTxt);
-
-			// Create HTML Formatter
-			formatterHTML = new MyHtmlFormatter();
-			fileHTML.setFormatter(formatterHTML);
-			logger.addHandler(fileHTML);
 		}
 
 		@Override
@@ -112,63 +98,6 @@ public class DesktopStarter {
 			Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).setLevel(Level.FINE);
 			Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).fine(
 					tag + ": " + message + " - " + exception.getMessage());
-		}
-	}
-
-	public static class MyHtmlFormatter extends Formatter {
-		// This method is called for every log records
-		public String format(LogRecord rec) {
-			if(rec.getLevel() == Level.ALL)
-				return "";
-			
-			StringBuffer buf = new StringBuffer(1000);
-			// Bold any levels >= WARNING
-			buf.append("<tr>");
-			buf.append("<td>");
-
-			if (rec.getLevel().intValue() >= Level.WARNING.intValue()) {
-				buf.append("<b>");
-				buf.append(rec.getLevel());
-				buf.append("</b>");
-			} else {
-				buf.append(rec.getLevel());
-			}
-			buf.append("</td>");
-			buf.append("<td>");
-			buf.append(calcDate(rec.getMillis()));
-			buf.append(' ');
-			buf.append(formatMessage(rec));
-			buf.append('\n');
-			buf.append("<td>");
-			buf.append("</tr>\n");
-			return buf.toString();
-		}
-
-		private String calcDate(long millisecs) {
-			SimpleDateFormat date_format = new SimpleDateFormat(
-					"MMM dd,yyyy HH:mm");
-			Date resultdate = new Date(millisecs);
-			return date_format.format(resultdate);
-		}
-
-		// This method is called just after the handler using this
-		// formatter is created
-		public String getHead(Handler h) {
-			if(h.getLevel() == Level.ALL)
-				return "";
-			return "<HTML>\n<HEAD>\n" + (new Date())
-					+ "\n</HEAD>\n<BODY>\n<PRE>\n"
-					+ "<table width=\"100%\" border>\n  "
-					+ "<tr><th>Level</th>" + "<th>Time</th>"
-					+ "<th>Log Message</th>" + "</tr>\n";
-		}
-
-		// This method is called just after the handler using this
-		// formatter is closed
-		public String getTail(Handler h) {
-			if(h.getLevel() == Level.ALL)
-				return "";
-			return "</table>\n  </PRE></BODY>\n</HTML>\n";
 		}
 	}
 }
