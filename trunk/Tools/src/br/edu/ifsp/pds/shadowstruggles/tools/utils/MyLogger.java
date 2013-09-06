bccdde3ca6b0fb2dcd5e3d9de20ab413ae0f4b44
gmerencio.santos@gmail.com <gmerencio.santos@gmail.com@ff7211c2-70b9-e154-77c6-c17a8a42dab7>
2013-09-06 16:49:38 +0000

    Log system updated.

--- trunk/Tools/src/br/edu/ifsp/pds/shadowstruggles/tools/utils/MyLogger.java
+++ trunk/Tools/src/br/edu/ifsp/pds/shadowstruggles/tools/utils/MyLogger.java
@@ -2,7 +2,6 @@ package br.edu.ifsp.pds.shadowstruggles.tools.utils;
 
 import java.io.IOException;
 import java.util.logging.FileHandler;
-import java.util.logging.Formatter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
@@ -14,25 +13,16 @@ public class MyLogger {
 	static private FileHandler fileTxt;
 	static private SimpleFormatter formatterTxt;
 
-	static private FileHandler fileHTML;
-	static private Formatter formatterHTML;
-
 	static public void setup() throws IOException {
-
 		// Get the global logger to configure it
 		Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-		logger.setLevel(Level.WARNING);
+		logger.setLevel(Level.INFO);
 		fileTxt = new FileHandler("Logging.txt", true);
-		fileHTML = new FileHandler("Logging.html", true);
+
 		// Create txt Formatter
 		formatterTxt = new SimpleFormatter();
 		fileTxt.setFormatter(formatterTxt);
 		logger.addHandler(fileTxt);
-
-		// Create HTML Formatter
-		formatterHTML = new MyHtmlFormatter();
-		fileHTML.setFormatter(formatterHTML);
-		logger.addHandler(fileHTML);
 	}
 }
