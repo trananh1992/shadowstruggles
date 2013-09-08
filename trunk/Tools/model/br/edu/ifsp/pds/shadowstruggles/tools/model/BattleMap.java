10746fc12f3c538be8ee631c3cbc6e90499fddeb
gmerencio.santos@gmail.com <gmerencio.santos@gmail.com@ff7211c2-70b9-e154-77c6-c17a8a42dab7>
2013-09-08 16:58:13 +0000

    Models updated (added BattleMap).

---
+++ trunk/Tools/model/br/edu/ifsp/pds/shadowstruggles/tools/model/BattleMap.java
@@ -0,0 +1,53 @@
+package br.edu.ifsp.pds.shadowstruggles.tools.model;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import br.edu.ifsp.pds.shadowstruggles.tools.data.SerializationHelper;
+
+import com.esotericsoftware.jsonbeans.Json;
+import com.esotericsoftware.jsonbeans.Json.Serializable;
+import com.esotericsoftware.jsonbeans.JsonValue;
+
+public class BattleMap implements Serializable {
+	public int id;
+	public String name;
+
+	public BattleMap() {
+		this.id = 1;
+		this.name = "";
+	}
+
+	public BattleMap(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	@Override
+	public void read(Json arg0, JsonValue arg1) {
+		try {
+			SerializationHelper.read(this, arg0, arg1, new ArrayList<String>());
+		} catch (IllegalArgumentException e) {
+			Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).severe(e.toString());
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).severe(e.toString());
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void write(Json arg0) {
+		try {
+			SerializationHelper
+					.writeToJson(this, arg0, new ArrayList<String>());
+		} catch (IllegalArgumentException e) {
+			Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).severe(e.toString());
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).severe(e.toString());
+			e.printStackTrace();
+		}
+	}
+
+}
