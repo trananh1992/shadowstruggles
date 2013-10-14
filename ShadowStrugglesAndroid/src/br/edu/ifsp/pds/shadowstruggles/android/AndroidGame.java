fcb15e3e0139ab0dfa7e4ed952ccf1fc50ca7618
gmerencio.santos <gmerencio.santos@gmail.com>
2013-10-14 21:05:10 +0000

    Fixes to Android build.

--- ShadowStrugglesAndroid/src/br/edu/ifsp/pds/shadowstruggles/android/AndroidGame.java
+++ ShadowStrugglesAndroid/src/br/edu/ifsp/pds/shadowstruggles/android/AndroidGame.java
@@ -1,12 +1,13 @@
 package br.edu.ifsp.pds.shadowstruggles.android;
 
 import br.edu.ifsp.pds.shadowstruggles.ShadowStruggles;
+import br.edu.ifsp.pds.shadowstruggles.ShadowStruggles.RunMode;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 
 public class AndroidGame extends AndroidApplication {
-        public void onCreate (android.os.Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                initialize(new ShadowStruggles(), false);
-        }
+	public void onCreate(android.os.Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		initialize(ShadowStruggles.getInstance(RunMode.DEBUG), false);
+	}
 }
