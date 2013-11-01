3321b4595cbe11c4a01298a36ad1c8c9e12840c1
gmerencio.santos@gmail.com <gmerencio.santos@gmail.com@ff7211c2-70b9-e154-77c6-c17a8a42dab7>
2013-11-01 23:33:29 +0000

    Model updates.

--- trunk/Tools/model/br/edu/ifsp/pds/shadowstruggles/tools/model/scenes/MovementControl.java
+++ trunk/Tools/model/br/edu/ifsp/pds/shadowstruggles/tools/model/scenes/MovementControl.java
@@ -6,23 +6,13 @@ import br.edu.ifsp.pds.shadowstruggles.tools.model.events.Event;
 public class MovementControl extends SceneItem {
 	public static enum Direction { NORTH, EAST, SOUTH, WEST };
 	
-	public Event targetEvent;
-	public boolean targetPlayer;
+	public Event target;
 	public Direction direction;
 	public int steps;
 	
 	public MovementControl() {
-		this.targetEvent = null;
-		this.targetPlayer = false;
-		this.direction = Direction.EAST;
+		this.target = null;
+		this.direction = Direction.NORTH;
 		this.steps = 0;
-	}
-	
-	public MovementControl(Event targetEvent, boolean targetPlayer,
-			Direction direction, int steps) {
-		this.targetEvent = targetEvent;
-		this.targetPlayer = targetPlayer;
-		this.direction = direction;
-		this.steps = steps;
 	}
 }
