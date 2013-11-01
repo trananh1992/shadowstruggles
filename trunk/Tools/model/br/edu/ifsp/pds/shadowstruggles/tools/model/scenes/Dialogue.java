7c5148ea5baee30d1dadc8d6d3ef8ffb00399f02
gmerencio.santos@gmail.com <gmerencio.santos@gmail.com@ff7211c2-70b9-e154-77c6-c17a8a42dab7>
2013-11-01 16:52:04 +0000

    Changes in Dialogue.

--- trunk/Tools/model/br/edu/ifsp/pds/shadowstruggles/tools/model/scenes/Dialogue.java
+++ trunk/Tools/model/br/edu/ifsp/pds/shadowstruggles/tools/model/scenes/Dialogue.java
@@ -3,17 +3,14 @@ package br.edu.ifsp.pds.shadowstruggles.tools.model.scenes;
 public class Dialogue extends SceneItem {
 	public String text;
 	public String characterName;
-	public String characterImage;
 	
 	public Dialogue() {
 		this.text = "";
 		this.characterName = "";
-		this.characterImage = "";
 	}
 	
 	public Dialogue(String text, String characterName, String characterImage) {
 		this.text = text;
 		this.characterName = characterName;
-		this.characterImage = characterImage;
 	}
 }
