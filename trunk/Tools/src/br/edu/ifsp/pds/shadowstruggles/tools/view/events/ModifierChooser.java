e7acb94b1a347acc19446f7df67ab919470da111
hugo.mitsumori@gmail.com <hugo.mitsumori@gmail.com@ff7211c2-70b9-e154-77c6-c17a8a42dab7>
2013-11-01 03:12:16 +0000

    Editor de EventActions sendo finalizado

--- trunk/Tools/src/br/edu/ifsp/pds/shadowstruggles/tools/view/events/ModifierChooser.java
+++ trunk/Tools/src/br/edu/ifsp/pds/shadowstruggles/tools/view/events/ModifierChooser.java
@@ -6,6 +6,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ModifierChooser extends JFrame {
 
@@ -31,12 +35,29 @@ public class ModifierChooser extends JFrame {
 	 * Create the frame.
 	 */
 	public ModifierChooser() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Modifier Chooser");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblChooseTheModifier = new JLabel("Choose the Modifier:");
+		lblChooseTheModifier.setBounds(37, 31, 110, 14);
+		contentPane.add(lblChooseTheModifier);
+		
+		JButton btnChooseModifier = new JButton("Choose Modifier");
+		btnChooseModifier.setBounds(37, 209, 121, 41);
+		contentPane.add(btnChooseModifier);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		});
+		btnCancel.setBounds(264, 209, 131, 41);
+		contentPane.add(btnCancel);
 	}
-
 }
--- trunk/Tools/src/br/edu/ifsp/pds/shadowstruggles/tools/view/events/SceneChooser.java
+++ trunk/Tools/src/br/edu/ifsp/pds/shadowstruggles/tools/view/events/SceneChooser.java
@@ -6,6 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class SceneChooser extends JFrame {
 
@@ -31,12 +36,37 @@ public class SceneChooser extends JFrame {
 	 * Create the frame.
 	 */
 	public SceneChooser() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setTitle("Scene Chooser");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 177);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblChooseAScene = new JLabel("Choose a Scene:");
+		lblChooseAScene.setBounds(38, 27, 99, 14);
+		contentPane.add(lblChooseAScene);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(172, 24, 144, 20);
+		contentPane.add(comboBox);
+		
+		JButton btnNewScene = new JButton("New Scene");
+		btnNewScene.setBounds(335, 23, 89, 23);
+		contentPane.add(btnNewScene);
+		
+		JButton btnChooseScene = new JButton("Choose Scene");
+		btnChooseScene.setBounds(37, 77, 130, 50);
+		contentPane.add(btnChooseScene);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		});
+		btnCancel.setBounds(308, 77, 105, 50);
+		contentPane.add(btnCancel);
 	}
-
 }
