c85b31bacade682fdf62c6d9d771675199615b8e
leonhpb <leonhpb@gmail.com>
2013-11-03 14:59:41 +0000

    new TexturePacker

---
+++ Shadow Struggles/tests/br/edu/ifsp/pds/shadowstruggles/dataTest/TexturePackerTest.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.pds.shadowstruggles.dataTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TexturePackerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
